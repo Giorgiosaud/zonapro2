31eb2465b35d7fb766689165bf0d5aa1f883ebce
Jorge Saud <jorgelsaud@gmail.com>
2018-03-02 19:40:59 -0300

    add sass

--- functions.php
+++ functions.php
@@ -1,2 +1,6 @@
 <?php
 require_once( 'wp-sass/wp-sass.php' );
+function my_theme_enqueue_styles() {
+	wp_enqueue_style( 'scss-style', get_stylesheet_directory_uri() . '/scss/style.scss.php' );
+}
+add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );

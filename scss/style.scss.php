eb511f36e72413268f9be2d940e001ad3cfe2c49
Jorge Saud <jorgelsaud@gmail.com>
2018-03-02 20:22:47 -0300

    fix logo

--- scss/style.scss.php
+++ scss/style.scss.php
@@ -1,6 +1,8 @@
 header.transparent .logo, .logo{
 	background-color: #49b2d8;
     background-position: center;
-    background-size: 80%;
+    background-size: 70%;
     border-radius: 50%;
+    width:70px;
+
 }

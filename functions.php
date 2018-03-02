6ba73e7a99c27ef822a976217f46ecb769bcc9c1
Jorge Saud <jorgelsaud@gmail.com>
2018-03-02 20:09:08 -0300

    add sass

--- functions.php
+++ functions.php
@@ -5,7 +5,7 @@ require_once( 'wp-sass/wp-sass.php' );
 // add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 add_action( 'after_setup_theme', 'your_parent_theme_setup', 9 );
 function your_parent_theme_setup() {    
-	wp_enqueue_style( 'scss-style', get_stylesheet_directory_uri() . '/scss/style.scss.php' );
+	wp_enqueue_style( 'scss-style', get_stylesheet_directory_uri() . '/scss/style.scss.php' ,array('rose-main-css'));
 
 	// global $aq_theme_options;
 	// die(var_dump(rose_get_option('rose_logo',false,true)));

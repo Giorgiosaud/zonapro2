e17fe0f3d03e700b4e0c96f6a059cd015e7c4555
Jorge Saud <jorgelsaud@gmail.com>
2018-03-02 19:54:16 -0300

    add sass

--- functions.php
+++ functions.php
@@ -6,6 +6,6 @@ function my_theme_enqueue_styles() {
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 add_action( 'after_setup_theme', 'your_parent_theme_setup', 9 );
 function your_parent_theme_setup() {    
-var_dump(rose_get_option('rose_logo'));
+	die(var_dump(rose_get_option('rose_logo',false,true)));
 }
 

888f5c10e61d67af419d9ff7d68d92d3661f5d11
Jorge Saud <jorgelsaud@gmail.com>
2018-03-02 19:47:45 -0300

    add sass

--- functions.php
+++ functions.php
@@ -4,19 +4,8 @@ function my_theme_enqueue_styles() {
 	wp_enqueue_style( 'scss-style', get_stylesheet_directory_uri() . '/scss/style.scss.php' );
 }
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
-function child_rose_get_option( $name, $default=false, $media = false ){
-  global $aq_theme_options;
-
-  $name = rose_get_prefix( $name );
-
-  if( isset( $aq_theme_options[ $name ] ) ){
-    $value =  $aq_theme_options[ $name ];
-    if( $media ){
-      return $value['url'];
-    }
-    return $value;
-  }
-
-  return $default;
-}
+add_action( 'after_setup_theme', 'your_parent_theme_setup', 9 );
+function your_parent_theme_setup() {    
 var_dump(child_rose_get_option('rose_logo'));
+}
+

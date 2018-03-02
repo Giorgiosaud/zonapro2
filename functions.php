2c0a9fa8faa1727b79dd76c7374b0c2b3938fd20
Jorge Saud <jorgelsaud@gmail.com>
2018-03-02 20:30:18 -0300

    fix logo

--- functions.php
+++ functions.php
@@ -6,7 +6,7 @@ function my_theme_enqueue_styles() {
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 add_action( 'after_setup_theme', 'your_parent_theme_setup', 9 );
 function your_parent_theme_setup() {    
-	die(var_dump(rose_get_option('logo',false,true)));
+	die(var_dump(rose_get_object_option('logo')));
 	wp_enqueue_style( 'scss-style', get_stylesheet_directory_uri() . '/scss/style.scss.php' ,array('rose-main'));
 
 }

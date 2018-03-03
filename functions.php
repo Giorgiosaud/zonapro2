7f3404825b5bba51550f5b8bd3330492022829e4
Jorge Saud <jorgelsaud@gmail.com>
2018-03-03 09:38:02 -0300

    DEBUG

--- functions.php
+++ functions.php
@@ -4,7 +4,7 @@ function my_theme_enqueue_styles() {
 	
 }
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
-add_action( 'after_setup_theme', 'your_parent_theme_setup', 9 );
+add_action( 'after_setup_theme', 'your_parent_theme_setup', 10 );
 function your_parent_theme_setup() {    
 	global $aq_theme_options;
 	(var_dump(

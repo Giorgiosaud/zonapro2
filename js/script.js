23efe8b161f05afb819d7bfcc84623405c2beb80
Jorge Saud <jorgelsaud@gmail.com>
2018-03-03 16:48:32 -0300

    remove menu movement to test

--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
- var hacerFunccionarMenu=function(){
- 	jQuery('li.menu-item.onePage a').click(function(e){
- 		if (/#/.test(this.href)) {
- 			e.preventDefault();
- 			var target = jQuery( jQuery(this).attr('href') );
- 			if( target.length ) {
- 				event.preventDefault();
- 				jQuery('html, body').animate({
- 					scrollTop: target.offset().top-75
- 				}, 500);
- 			}
- 		}
- 	})
- }
- setTimeout(hacerFunccionarMenu, 10000);
+ // var hacerFunccionarMenu=function(){
+ // 	jQuery('li.menu-item.onePage a').click(function(e){
+ // 		if (/#/.test(this.href)) {
+ // 			e.preventDefault();
+ // 			var target = jQuery( jQuery(this).attr('href') );
+ // 			if( target.length ) {
+ // 				event.preventDefault();
+ // 				jQuery('html, body').animate({
+ // 					scrollTop: target.offset().top-75
+ // 				}, 500);
+ // 			}
+ // 		}
+ // 	})
+ // }
+ // setTimeout(hacerFunccionarMenu, 10000);

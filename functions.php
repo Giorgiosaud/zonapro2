50671fbae4182907a399f4516bd215702b080f44
Jorge Saud <jorgelsaud@gmail.com>
2018-03-03 09:33:39 -0300

    DEBUG

--- functions.php
+++ functions.php
@@ -6,7 +6,8 @@ function my_theme_enqueue_styles() {
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 add_action( 'after_setup_theme', 'your_parent_theme_setup', 9 );
 function your_parent_theme_setup() {    
-	(var_dump(rose_get_object_option('logo', get_template_directory_uri().'/assets/images/logo.png', true)));
+	(var_dump(
+	$aq_theme_options));
 	wp_enqueue_style( 'scss-style', get_stylesheet_directory_uri() . '/scss/style.scss.php' ,array('rose-main'));
 
 }

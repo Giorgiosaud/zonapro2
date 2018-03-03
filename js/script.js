2ae3c4503f6a4a072716f25a1e6984d952d29988
Jorge Saud <jorgelsaud@gmail.com>
2018-03-03 11:11:30 -0300

    click menu

--- js/script.js
+++ js/script.js
@@ -1,9 +1,15 @@
 jQuery(document).ready(function($) {
 	// CLICK MENU ONE PAGE  --------------------------
-	$('.primary-navigation li.menu-item').click(function(){
+	$('.primary-navigation li.menu-item a').click(function(e){
 		if (/#/.test(this.href)) {
-			console.log(this);
 			e.preventDefault();
+			var target = $( $(this).attr('href') );
+			if( target.length ) {
+				event.preventDefault();
+				$('html, body').animate({
+					scrollTop: target.offset().top-75
+				}, 500);
+			}
 		}
 	})
 });

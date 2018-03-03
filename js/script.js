c51df183323b5a1536612b61dc4b7d4137117ec5
Jorge Saud <jorgelsaud@gmail.com>
2018-03-03 16:42:18 -0300

    enable menu movement

--- js/script.js
+++ js/script.js
@@ -2,9 +2,9 @@ jQuery(document).ready(function($) {
 	// CLICK MENU ONE PAGE  --------------------------
 	$('#nav-menu > li.menu-item.onePage > a').click(function(e){
 		if (/#/.test(this.href)) {
-			e.preventDefault();
 			var target = $( $(this).attr('href') );
 			if( target.length ) {
+				e.preventDefault();
 				event.preventDefault();
 				$('html, body').animate({
 					scrollTop: target.offset().top-75

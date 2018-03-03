8742efe9dffe11753fc78af3e1d721435799cec5
Jorge Saud <jorgelsaud@gmail.com>
2018-03-03 09:35:26 -0300

    DEBUG

--- functions.php
+++ functions.php
@@ -6,6 +6,7 @@ function my_theme_enqueue_styles() {
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 add_action( 'after_setup_theme', 'your_parent_theme_setup', 9 );
 function your_parent_theme_setup() {    
+	global $aq_theme_options;
 	(var_dump(
 	$aq_theme_options));
 	wp_enqueue_style( 'scss-style', get_stylesheet_directory_uri() . '/scss/style.scss.php' ,array('rose-main'));

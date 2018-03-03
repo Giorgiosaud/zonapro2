9cf3dbfec29eaa8fc9f28af4f9bf695afff144b1
Jorge Saud <jorgelsaud@gmail.com>
2018-03-03 11:18:46 -0300

    click menu

--- js/script.js
+++ js/script.js
@@ -1,14 +1,10 @@
 jQuery(document).ready(function($) {
 	// CLICK MENU ONE PAGE  --------------------------
-	$('.primary-navigation li.menu-item a').click(function(e){
+	$('.primary-navigation li.menu-item.onePage a').click(function(e){
 		if (/#/.test(this.href)) {
-				e.preventDefault();
-			
-			var target = $( $(this).attr('href') ),
-			tlength=$(this).attr('href');
-
-			console.log(target);
-			if( tlength.length>1 ) {
+			e.preventDefault();
+			var target = $( $(this).attr('href') );
+			if( target.length ) {
 				event.preventDefault();
 				$('html, body').animate({
 					scrollTop: target.offset().top-75
